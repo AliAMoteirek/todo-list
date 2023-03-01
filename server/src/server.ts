e4de4d3bb8057ca10602d49d8a60304a6f0d3979
Ali Moteirek <mtairek20@gmail.com>
2023-03-01 12:15:42 +0100

    added envalid

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,21 @@
 import express from 'express';
+import 'dotenv/config';
+import mongoose from 'mongoose';
+import env from './utils/validateEnv';
 
 const app = express();
-const port = 8080;
 
 app.get('/', (req, res) => {
   res.send({ msg: 'Hello, World!!' });
 });
 
-app.listen(port, () => {
-  console.log(`The server is running on port: ${port}`);
-});
+const port = env.PORT;
+mongoose
+  .connect(env.MONGO_CONNECTION_SRTING)
+  .then(() => {
+    console.log('MongoDB Connected');
+    app.listen(port, () => {
+      console.log(`The server is running on port: ${port}`);
+    });
+  })
+  .catch(console.error);
